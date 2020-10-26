22de038feb3673bb2192860e133330ed655ab034
Christian Francis <cfcoding@yahoo.com>
2020-10-26 11:52:32 -0230

    Initial Commit

---
+++ KidsMoney-Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidsMoney-Programmatic
+//
+//  Created by Christian Elijah on 2020-10-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
